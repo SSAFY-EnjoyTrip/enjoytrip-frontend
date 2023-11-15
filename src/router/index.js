b6f24b2f6d38a78ccda92a445ad999b1bbdb61a7
mj0107 <alswns1285@gmail.com>
2023-11-15 14:41:52 +0900

    Feat: 라우터 경로 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+
+import HomeView from '../views/HomeView.vue';
+import AttractionView from '../views/AttractionView.vue';
+import PlanView from '../views/PlanView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/attractions',
+    name: 'attractions',
+    component: AttractionView,
+  },
+  {
+    path: '/plans',
+    name: 'plans',
+    component: PlanView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  routes,
 });
 
 export default router;
