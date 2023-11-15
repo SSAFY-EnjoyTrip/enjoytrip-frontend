ea54c73babe399edfd0c12839c15588235ae9758
mj0107 <alswns1285@gmail.com>
2023-11-15 14:36:30 +0900

    Feat: view 라우팅 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+import TheNavbar from './layout/TheNavbar.vue';
 </script>
 
 <template>
-  <router-view></router-view>
+  <div class="q-pa-md">
+  <q-layout view="lHr Lpr lFr">
+    <TheNavbar />
+    <q-page-container>
+      <q-page padding>
+        <router-view />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</div>
 </template>
 
 <style scoped>
