1345440004ec9086689f99ee5222e1b5c9f204be
mj0107 <alswns1285@gmail.com>
2023-11-15 14:33:35 +0900

    Feat: 상단 네비게이션 바 추가
    - quasar에서 제공하는 라우터 태그 이용해서 라우팅 기능도 추가

---
+++ src/layout/TheNavbar.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-tabs align="left">
+      <q-route-tab to="/" label="Home" />
+      <q-route-tab to="attractions" label="Attractions" />
+      <q-route-tab to="plans" label="Plans" />
+
+      <q-space />
+
+      <q-route-tab to="" label="Login" />
+      <q-route-tab to="" label="Sign in" />
+    </q-tabs>
+  </q-header>
+</template>
+
+<style scoped></style>
