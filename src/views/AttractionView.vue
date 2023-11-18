2cb97dbc6977c18972ed04659f40d79bef765aa3
mj0107 <alswns1285@gmail.com>
2023-11-18 21:52:21 +0900

    Feat: 선택한 관광지 provide 기능 추가
    - AttractionListItem 컴포넌트에서 선택한 관광지 형제 컴포넌트에서 사용할 수 있도록 최상위에서 provide 하도록 추가
    - 기존에 KakaoMap 컴포넌트의 위치 AttractionList에서 상위 view로 이동

--- src/views/AttractionView.vue
+++ src/views/AttractionView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { provide, ref } from 'vue';
+
 import AttractionList from '../components/Attraction/AttractionList.vue';
+import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+
+const selectedAttraction = ref(null);
+
+provide('selectedAttraction', selectedAttraction);
 </script>
 
 <template>
+  <KakaoMap />
   <AttractionList />
 </template>
 
