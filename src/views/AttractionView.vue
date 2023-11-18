2e927169f7236771707922b50260dff5bbffaa5f
mj0107 <alswns1285@gmail.com>
2023-11-19 02:30:06 +0900

    Feat: 하위에서 사용할 변수들 provide
    - 하위 컴포넌트들의 상태 변수와 함수들 모두 inject 받아서 사용하도록 provide 설정

--- src/views/AttractionView.vue
+++ src/views/AttractionView.vue
@@ -1,16 +1,67 @@
 <script setup>
-import { provide, ref } from 'vue';
+import { provide, ref, watch } from 'vue';
+import axios from 'axios';
 
-import AttractionList from '../components/Attraction/AttractionList.vue';
 import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+import AttractionSearch from '../components/Attraction/AttractionSearch.vue';
+import AttractionList from '../components/Attraction/AttractionList.vue';
 
+const attractions = ref([]);
 const selectedAttraction = ref(null);
+const selectedSido = ref(null);
+const selectedGugun = ref(null);
+const keyword = ref(null);
+const page = ref(1);
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const LIMIT = import.meta.env.VITE_ATTRACTION_LIST_LIMIT;
+
+const getAttractions = async ($state) => {
+  try {
+    const res = await axios.get(
+      `${BASE_URL}/attractions/list`, {
+        params: {
+          page: page.value,
+          limit: LIMIT,
+          sidoCode: selectedSido.value,
+          gugunCode: selectedGugun.value,
+          keyword: keyword.value,
+        }
+      }
+    );
+    const json = await res.data.attractions;
+
+    if (json.length < LIMIT) $state.complete();
+    else {
+      attractions.value.push(...json);
+      $state.loaded();
+    }
+    page.value++;
+  } catch (error) {
+    $state.error();
+  }
+};
+
+watch([selectedSido, selectedGugun, keyword], () => {
+  attractions.value = [];
+  page.value = 1;
+  console.log(`selectedSido: ${selectedSido.value}, selectedGugun: ${selectedGugun.value}, keyword: ${keyword.value}`);
+  getAttractions({ loaded() {}, error() {} });
+});
+
+provide('attractions', attractions);
 provide('selectedAttraction', selectedAttraction);
+provide('getAttractions', getAttractions);
+provide('page', page);
+
+provide('selectedSido', selectedSido);
+provide('selectedGugun', selectedGugun);
+provide('keyword', keyword);
 </script>
 
 <template>
   <KakaoMap />
+  <AttractionSearch />
   <AttractionList />
 </template>
 
