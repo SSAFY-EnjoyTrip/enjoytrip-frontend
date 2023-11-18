dbe2009407292806e3a43c6acf6dfa1594ee2e8c
mj0107 <alswns1285@gmail.com>
2023-11-19 04:32:38 +0900

    Feat: 회원가입창은 Navbar가 안보이도록 설정

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 import TheNavbar from './layout/TheNavbar.vue';
 
 const route = useRoute();
+const invisiableRouter = ref(['/login', '/signup']);
 </script>
 
 <template>
   <div class="">
   <q-layout view="lHr Lpr lFr">
-    <TheNavbar v-if="route.path !== '/login'"/>
+    <TheNavbar v-if="!invisiableRouter.includes(route.path)" />
     <q-page-container>
       <q-page class="q-px-xl">
         <router-view />
