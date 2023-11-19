221f176d12aaa3ae7c8552dac9ce12e2d0e2e769
mj0107 <alswns1285@gmail.com>
2023-11-19 19:19:44 +0900

    Feat: 라우터가 준비되면 마운트되도록 설정

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { Quasar, SessionStorage, Notify } from 'quasar';
 
 import App from './App.vue';
 import router from './router';
 
 // Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons/material-icons.css';
 
 // Import Quasar css
-import 'quasar/dist/quasar.css'
+import 'quasar/dist/quasar.css';
 
 const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(Quasar, {
   plugins: {
@@ -22,4 +25,6 @@ app.use(Quasar, {
   },
 });
 
-app.mount('#app');
+router.isReady().then(() => {
+  app.mount('#app');
+});
