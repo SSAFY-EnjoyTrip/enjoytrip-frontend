d07a99c29d0bf5cd0a861a08f0c9f8cb471e9cbe
mj0107 <alswns1285@gmail.com>
2023-11-20 10:59:31 +0900

    Feat: PlanVIew UI 추가

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,11 +1,76 @@
 <script setup>
-import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+import { ref } from 'vue';
+
+import PlanSearch from '../components/Plan/PlanSearch.vue';
+import PlanCard from '../components/Plan/PlanCard.vue';
+
+const options = ref(['최신순', '좋아요순']);
+const model = ref(options.value[0]);
 </script>
 
 <template>
-  <KakaoMap />
+  <q-page class="q-pa-xl">
+    <div class="text-h5 q-my-lg">플랜을 검색해보세요</div>
+    <PlanSearch />
+
+    <div class="q-my-xl">
+      <div class="text-h5">Top Weekly</div>
+      <div class="row">
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+      <div class="row">
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+    </div>
+
+    <div class="q-my-xl">
+      <div class="text-h5">Top Monthly</div>
+      <div class="row">hhh
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+      <div class="row">
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+    </div>
+
+    <div class="q-my-xl">
+      <div class="text-h5">Top Yearly</div>
+      <div class="row">
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+      <div class="row">h
+        <PlanCard class="col" v-for="n in 4" :key="n" />
+      </div>
+    </div>
+
+    <div class="q-my-xl">
+      <div class="row">
+        <div class="text-h5">Plans</div>
+        <q-space />
+        <q-select
+          outlined
+          v-model="model"
+          :options="options"
+          label="정렬 순서"
+          behavior="menu"
+        />
+      </div>
+      <div class="row">
+        <div class="row">
+          <PlanCard class="col" v-for="n in 4" :key="n" />
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <style scoped>
-
+.q-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.q-select {
+  width: 200px;
+}
 </style>
