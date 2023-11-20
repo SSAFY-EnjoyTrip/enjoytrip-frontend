c0f49db48f9cd0f3f8bd4d85e2edda35ccbc717f
mj0107 <alswns1285@gmail.com>
2023-11-20 10:59:09 +0900

    Feat: PlanSearch UI 추가

---
+++ src/components/Plan/PlanSearch.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const keyword = ref('');
+</script>
+
+<template>
+  <q-input v-model="keyword" type="search" rounded outlined>
+    <template #append>
+      <q-icon name="search" class="cursor-pointer" />
+    </template>
+  </q-input>
+</template>
+
+<style scoped>
+.q-input {
+  width: 50%;
+}
+</style>
