0d2a708544570ad70b2065bbd0c3de0922412a2c
mj0107 <alswns1285@gmail.com>
2023-11-20 09:28:34 +0900

    Feat: PlanDetailList 추가

---
+++ src/components/PlanDetail/PlanDetailList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PlanDetailListItem from './PlanDetailListItem.vue';
+</script>
+
+<template>
+  <PlanDetailListItem v-for="n in 5" :key="n"/>
+</template>
+
+<style scoped>
+
+</style>
