5707f44b5b404ad644e44a5d693c16e298459df3
mj0107 <alswns1285@gmail.com>
2023-11-20 09:29:03 +0900

    Feat: PlanDetailView 추가

---
+++ src/views/PlanDetailView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+import PlanDetail from '../components/PlanDetail/PlanDetail.vue';
+</script>
+
+<template>
+  <q-page class="row">
+    <div class="col-5">
+      <PlanDetail />
+    </div>
+    <div class="col-7">
+      <KakaoMap style="height: 100vh" />
+    </div>
+  </q-page>
+</template>
+
+<style scoped></style>
