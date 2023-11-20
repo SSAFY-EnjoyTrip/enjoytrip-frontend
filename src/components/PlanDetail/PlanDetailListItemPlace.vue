6b750397cd517b4aa031f4940cc18617b95f01fe
mj0107 <alswns1285@gmail.com>
2023-11-20 09:27:04 +0900

    Feat: PlanDetailListIemPlace 추가

---
+++ src/components/PlanDetail/PlanDetailListItemPlace.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <div class="q-mb-md row align-center">
+    <q-icon name="place" class="marker" />
+    <span class="text-h5">Place Name</span>
+  </div>
+  <div class="row">
+    <div class="col-8">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+      ratione ipsam officiis velit itaque, tenetur ipsa incidunt voluptatum
+      quasi maxime molestiae nesciunt, voluptas numquam! Perferendis itaque quam
+      aspernatur inventore! Eum.
+    </div>
+    <div class="col-4">
+      <q-img fit="cover" src="https://picsum.photos/500/300" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.marker {
+  font-size: 32px;
+  color: rgba(0, 0, 0, 0.5);
+}
+</style>
