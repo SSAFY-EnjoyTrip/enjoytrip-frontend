9614a9c49e3831df3a38b324ac4a5c56a279f9e1
mj0107 <alswns1285@gmail.com>
2023-11-20 10:58:51 +0900

    Feat: PlanCard UI 추가

---
+++ src/components/Plan/PlanCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-card class="q-ma-md" flat bordered>
+    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+    <q-card-section>
+      <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+      <div class="text-caption text-grey">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style scoped>
+.q-card {
+  max-width: 300px;
+}
+</style>
