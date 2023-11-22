7a21d1417840912726120d3d7a6e2fcd6a08ccfa
mj0107 <alswns1285@gmail.com>
2023-11-22 14:29:47 +0900

    Feat: PlanRegistView 추가

---
+++ src/views/PlanRegistView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, provide } from 'vue';
+
+import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+import PlanRegistDetail from '../components/PlanRegistDetail/PlanRegistDetail.vue';
+
+const attractionList = ref([]);
+
+provide('attractionList', attractionList);
+</script>
+
+<template>
+  <q-page class="row">
+    <div class="col-5">
+      <PlanRegistDetail />
+    </div>
+    <div class="col-7">
+      <KakaoMap style="height: 100vh" />
+    </div>
+  </q-page>
+</template>
+
+<style scoped></style>
