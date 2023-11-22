3ff7e36fa9eaa85eee2b2556cdaabbe7e54eb811
mj0107 <alswns1285@gmail.com>
2023-11-22 14:23:58 +0900

    Feat: 플랜 등록시 관광지 추가 기능 추가가

--- src/components/Attraction/AttractionListItem.vue
+++ src/components/Attraction/AttractionListItem.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, inject } from 'vue';
+import { useQuasar } from 'quasar';
 
 const props = defineProps({
   attraction: {
@@ -8,12 +9,52 @@ const props = defineProps({
   },
 });
 
+const isPopup = inject('isPopup');
+
 const selectedAttraction = inject('selectedAttraction');
+const attractionList = inject('attractionList');
 const attraction = ref(props.attraction);
+
+const $q = useQuasar();
 
 const setAttraction = () => {
   selectedAttraction.value = attraction.value;
-}
+};
+const addAttraction = () => {
+  console.log('before: ', attractionList.value);
+  if (attractionList.value.some((el) => el.contentId === attraction.value.contentId)) {
+    // $q.notify({
+    //   type: 'warning',
+    //   message: '이미 마지막에 추가한 관광지입니다. 정말 추가하시겠습니까?',
+    //   actions: [
+    //     {
+    //       label: '예',
+    //       color: 'primary',
+    //       handler: () => {
+    //         attractionList.value.push(attraction.value);
+    //         $q.notify({
+    //           type: 'primary',
+    //           message: `'${attraction.value.title}'이 추가되었습니다.`,
+    //         });
+    //       },
+    //     },
+    //     { label: '아니오', color: 'primary' },
+    //   ],
+    // });
+    $q.notify({
+      type: 'negative',
+      message: '이미 추가한 관광지입니다.',
+    })
+  } else {
+    attractionList.value = [...attractionList.value, attraction.value];
+    $q.notify({
+      type: 'primary',
+      message: `<b>'${attraction.value.title}'</b> 이 추가되었습니다.`,
+      html: true,
+    });
+  }
+  console.log('after: ', attractionList.value);
+};
 </script>
 
 <template>
@@ -34,6 +75,15 @@ const setAttraction = () => {
     />
 
     <q-card-section>
+      <q-btn
+        v-if="isPopup"
+        fab
+        color="primary"
+        icon="add"
+        class="absolute"
+        style="top: 0; right: 80px; transform: translateY(-50%)"
+        @click="addAttraction"
+      />
       <q-btn
         fab
         color="primary"
