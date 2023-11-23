c18b8a2ca937d6fbfaa9bb5881dcd5cb016077c4
mj0107 <alswns1285@gmail.com>
2023-11-23 19:55:24 +0900

    Feat: MyPageTabMyPlan 구현

---
+++ src/components/MyPage/MyPageTabMyPlan.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref, inject } from 'vue';
+import axios from 'axios';
+
+import MyPageTabCard from './MyPageTabCard.vue';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const userInfo = inject('userInfo');
+const userId = userInfo.value.id;
+
+const myPlanList = ref([]);
+
+const getMyPlanList = async () => {
+  const res = await axios.get(`${BASE_URL}/plans/myplan/${userId}`);
+  const data = await res.data;
+
+  myPlanList.value = data;
+}
+
+onMounted(() => {
+  getMyPlanList();
+})
+</script>
+
+<template>
+  <div class="row justify-around">
+    <MyPageTabCard v-for="plan in myPlanList" :key="plan.id" :plan="plan" class="col-5 q-pa-lg" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
