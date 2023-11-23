e9dffe770069439bfbc4762bd400338205a12ae9
mj0107 <alswns1285@gmail.com>
2023-11-23 20:02:36 +0900

    Feat: MyPageView 추가

---
+++ src/views/MyPageView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, provide } from 'vue';
+import { storeToRefs } from 'pinia';
+
+import { useMemberStore } from '../stores/member';
+
+import MyPageProfile from '../components/Mypage/MyPageProfile.vue';
+import MyPageTab from '../components/MyPage/MyPageTab.vue';
+import KakaoMap from '../components/KakaoMap/KakaoMap.vue';
+
+const memberStore = useMemberStore();
+const selectedPlan = ref(null);
+
+const { userInfo } = storeToRefs(memberStore);
+const { getUserInfo } = memberStore;
+
+provide('parentView', 'MyPageView');
+provide('userInfo', userInfo);
+provide('getUserInfo', getUserInfo);
+provide('selectedPlan', selectedPlan);
+</script>
+
+<template>
+  <q-page class="q-pa-xl row justify-between">
+    <div class="col-4">
+      <MyPageProfile />
+    </div>
+
+    <q-separator vertical color="blue-1" size="2px" />
+
+    <div class="col-7">
+      <KakaoMap />
+      <MyPageTab />
+    </div>
+  </q-page>
+</template>
+
+<style scoped></style>
