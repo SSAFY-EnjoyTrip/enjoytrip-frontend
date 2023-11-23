f5e2119c32b3524f5320be2012bb4f967ba904e7
mj0107 <alswns1285@gmail.com>
2023-11-23 19:55:55 +0900

    Feat: PlanCard 구현

--- src/components/Plan/PlanCard.vue
+++ src/components/Plan/PlanCard.vue
@@ -1,12 +1,41 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const props = defineProps({
+  plan: Object,
+});
+
+const router = useRouter();
+
+const viewDetailHandler = () => {
+  router.push({
+    path: `/plans/${props.plan.id}`,
+    state: {
+      plan: { ...props.plan },
+    },
+  });
+};
+</script>
+
 <template>
-  <q-card class="q-ma-md" flat bordered>
-    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+  <q-card class="q-ma-md" flat bordered @click="viewDetailHandler">
+    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <div class="absolute-bottom">
+        <q-icon
+          name="favorite_border"
+          size="20px"
+          color="red"
+          class="q-mr-xs"
+        />
+        {{ plan.likeCount }}
+      </div>
+    </q-img>
 
     <q-card-section>
-      <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+      <div class="text-h5 q-mt-sm q-mb-xs">{{ plan.title }}</div>
       <div class="text-caption text-grey">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+        {{ plan.content }}
       </div>
     </q-card-section>
   </q-card>
