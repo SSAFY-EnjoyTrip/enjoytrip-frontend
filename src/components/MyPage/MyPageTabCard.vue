bf4b0e6a00a146eeccc71e21c2ae418ef0195d84
mj0107 <alswns1285@gmail.com>
2023-11-24 04:52:35 +0900

    Feat: 수정 페이지로 가는 기능 추가

--- src/components/MyPage/MyPageTabCard.vue
+++ src/components/MyPage/MyPageTabCard.vue
@@ -3,14 +3,31 @@ import { useRouter } from 'vue-router';
 
 const props = defineProps({
   plan: Object,
+  type: {
+    type: String,
+    default: null,
+  }
 });
 
 const plan = props.plan;
+const type = props.type;
 
 const router = useRouter();
+
 const viewDetailHandler = () => {
   router.push({
     path: `/plans/${props.plan.id}`,
+    state: {
+      plan: { ...props.plan },
+    },
+  });
+};
+
+const viewEditDetailHandler = (e) => {
+  e.stopPropagation();
+
+  router.push({
+    path: `/plans/edit/${props.plan.id}`,
     state: {
       plan: { ...props.plan },
     },
@@ -24,9 +41,10 @@ const viewDetailHandler = () => {
       <q-icon name="place" class="marker" />
       <span class="text-h5">{{ plan.title }}</span>
       <q-space />
+      <q-btn v-if="type === 'myplan'" icon="edit" @click="(e) => viewEditDetailHandler(e)" class="z-top" />
     </div>
     <div class="row">
-      <div class="col-8">
+      <div class="col-8 ellipsis">
         {{ plan.content }}
       </div>
       <div class="col-4">
