eb6db97349247f17c0cf731b022d9e5477437065
mj0107 <alswns1285@gmail.com>
2023-11-23 19:56:15 +0900

    Feat: PlanDetail 구현
    - 플랜 세부정보

--- src/components/PlanDetail/PlanDetail.vue
+++ src/components/PlanDetail/PlanDetail.vue
@@ -1,16 +1,46 @@
 <script setup>
 import PlanDetailList from './PlanDetailList.vue';
+
+import { ref, onMounted, inject } from 'vue';
+import axios from 'axios';
+
+const { plan } = history.state;
+const attractionList = inject('attractionList');
+const commentList = ref([]);
+const planWriterInfo = ref({});
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const getPlanDetailList = async () => {
+  const res = await axios.get(`${BASE_URL}/plans/${plan.id}`);
+  const data = await res.data;
+
+  attractionList.value = await data.planDetail.map((el) => el.attractionDto);
+  commentList.value = data.commentList;
+};
+
+const getPlanWriterInfo = async () => {
+  const res = await axios.get(`${BASE_URL}/users/info?userId=${plan.userId}`);
+  const data = await res.data;
+
+  planWriterInfo.value = await data.userInfo;
+};
+
+onMounted(() => {
+  getPlanDetailList();
+  getPlanWriterInfo();
+});
 </script>
 
 <template>
   <div style="overflow: auto; max-height: 100vh">
     <q-card class="plan-img">
       <q-icon
-          name="bookmark_border"
-          class="text-h4 bookmark absolute-top-right z-top"
-        />
+        name="bookmark"
+        class="text-h4 bookmark absolute-top-right z-top"
+      />
       <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="text-h5 absolute-bottom text-left">Title</div>
+        <div class="text-h5 absolute-bottom text-left">{{ plan.title }}</div>
       </q-img>
     </q-card>
 
@@ -20,9 +50,14 @@ import PlanDetailList from './PlanDetailList.vue';
           <img src="https://cdn.quasar.dev/img/avatar.png" />
         </q-avatar>
         <div class="q-mr-xl">
-          <div class="name text-h5">김민준</div>
-          <div class="date text-h6">2023년 11월 19일</div>
+          <div class="name text-h5">{{ planWriterInfo.nickname }}</div>
+          <div class="date text-h6">
+            {{ new Date(plan.createdAt).toLocaleDateString() }}
+          </div>
         </div>
+
+        <q-space />
+
         <div class="infos col-2 row justify-between">
           <div>
             <q-icon
@@ -31,7 +66,7 @@ import PlanDetailList from './PlanDetailList.vue';
               color="red"
               class="q-mr-xs"
             />
-            <span class="like-count">999</span>
+            <span class="like-count">{{ plan.likeCount }}</span>
           </div>
           <div>
             <q-icon
@@ -40,22 +75,21 @@ import PlanDetailList from './PlanDetailList.vue';
               color="blue"
               class="q-mr-xs"
             />
-            <span class="like-count">999</span>
+            <span class="like-count">{{ commentList.length }}</span>
           </div>
         </div>
         <q-space />
-        <div>
-          <q-btn>Follow</q-btn>
-        </div>
       </div>
-      <q-separator class="q-my-lg" />
-      <p class="text-h6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae libero
-        accusantium reiciendis exercitationem ex aliquid dolores porro
-        reprehenderit ratione obcaecati dolore ipsa quam, eos earum nam veniam
-        delectus maiores in!
-      </p>
-      <PlanDetailList />
+      <q-separator class="q-mt-lg" />
+      <div class="q-pa-xl">
+        <div>
+          <div class="text-h5 q-mb-md">계획 설명</div>
+          <div>
+            <q-input v-model="plan.content" filled autogrow readonly />
+          </div>
+        </div>
+        <PlanDetailList />
+      </div>
     </div>
   </div>
 </template>
@@ -69,8 +103,11 @@ import PlanDetailList from './PlanDetailList.vue';
   display: flex;
   align-items: center;
 }
+.bookmark {
+  color: rgba(0, 0, 0, 0.3);
+}
 .bookmark:hover {
-  color: white;
+  color: yellow;
   cursor: pointer;
 }
 </style>
