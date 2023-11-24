b471b4a5c75d910e1b26611f62d6da53c0010e34
Suhyeon Lee <suhyeon9120@gmail.com>
2023-11-24 09:00:25 +0900

    Feat: HomePlanCard 추가

---
+++ src/components/Home/HomePlanCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const props = defineProps({
+  plan: Object,
+});
+
+const router = useRouter();
+
+const viewDetailHandler = () => {
+  router.push({
+    path: `/plans/${props.plan.id}`,
+    state: {
+      plan: { ...props.plan },
+    },
+  });
+};
+</script>
+
+<template>
+  <q-card @click="viewDetailHandler">
+    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <div class="absolute-bottom text-subtitle2 text-center">
+        {{ plan.title }}
+      </div>
+    </q-img>
+  </q-card>
+</template>
+
+<style scoped>
+.q-card {
+  max-width: 50px;
+}
+</style>
